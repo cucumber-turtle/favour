715ac4f167e6fd3b2288fec78b2437d3528fffb0
Joud <asfarijoud@ecs.vuw.ac.nz>
2022-09-03 23:37:34 +1200

    Firebase auth and using contexts.

--- src/AppInit.js
+++ src/AppInit.js
@@ -1,9 +1,27 @@
 import * as React from 'react';
 import * as SecureStore from 'expo-secure-store';
+import { getAuth, signInWithCustomToken, signInWithEmailAndPassword, verifyIdToken } from "firebase/auth";
+
+// get authentication token from firebase and save to usertoken
+function authenticate (email, password) {
+    // Authenticate using firebase and get token
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        return user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+}
 
 // Authentication flow referenced from: https://reactnavigation.org/docs/auth-flow/
 
-const AuthContext = React.createContext();
+// AuthContext to expose needed methods
+export const AuthContext = React.createContext('');
 
 export default function AppInit () {
        const [state, dispatch] = React.useReducer(
@@ -47,36 +65,34 @@ export default function AppInit () {
               // Restoring token failed
             }
 
-            // After restoring token, we may need to validate it in production apps
-
-            // This will switch to the App screen or Auth screen and this loading
-            // screen will be unmounted and thrown away.
-            dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+            // After restoring token validate it in firebase
+            verifyIdToken(userToken.getIdToken())
+            .then(function(decodedToken) {
+                var uid = decodedToken.uid;
+                dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+              }).catch(function(error) {
+                // OOPS, an error...
+              });
           };
 
           bootstrapAsync();
             }, []);
 
-    const authContext = React.useMemo(
+       // usememo to keep expensive functions from needlessly running
+       const authContext = React.useMemo(
         () => ({
-          signIn: async (data) => {
-            // In a production app, we need to send some data (usually username, password) to server and get a token
-            // We will also need to handle errors if sign in failed
-            // After getting token, we need to persist the token using `SecureStore`
-            // In the example, we'll use a dummy token
+           isAuthenticated: false,
+           signIn: async (name, pass) => {
+               const signToken = authenticate(name, pass);
+               dispatch({type: 'SIGN_IN', token: signToken});
+           },
+           signOut: () => dispatch({type: 'SIGN_OUT'}),
+           signUp: async (name, pass) => {
+               const signToken = authenticate(name, pass);
+               dispatch({type: 'SIGN_IN', token: signToken});
+           }
+        }), []
+       );
 
-            dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
-          },
-          signOut: () => dispatch({ type: 'SIGN_OUT' }),
-          signUp: async (data) => {
-            // In a production app, we need to send user data to server and get a token
-            // We will also need to handle errors if sign up failed
-            // After getting token, we need to persist the token using `SecureStore`
-            // In the example, we'll use a dummy token
-
-            dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
-          },
-        }),
-        []
-      );
+       return authContext;
 }
